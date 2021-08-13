ce1f16cc60326d7fe4388eb4d2e7aa71e799bb27
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-08-13 20:17:39 +0200

    Avoid conflict between `,` and `]`

--- src/Sniff/PunctuationSpacingSniff.php
+++ src/Sniff/PunctuationSpacingSniff.php
@@ -11,6 +11,10 @@ use TwigCsFixer\Token\Token;
  */
 final class PunctuationSpacingSniff extends AbstractSpacingSniff
 {
+    private const NO_SPACE_BEFORE = [')', ']', '}', ':', '.', ',', '|'];
+    private const NO_SPACE_AFTER = ['(', '[', '{', '.', '|'];
+    private const ONE_SPACE_AFTER = [':', ','];
+
     /**
      * @param int               $tokenPosition
      * @param array<int, Token> $tokens
@@ -20,7 +24,7 @@ final class PunctuationSpacingSniff extends AbstractSpacingSniff
     protected function shouldHaveSpaceBefore(int $tokenPosition, array $tokens): ?int
     {
         $token = $tokens[$tokenPosition];
-        if ($this->isTokenMatching($token, Token::PUNCTUATION_TYPE, [')', ']', '}', ':', '.', ',', '|'])) {
+        if ($this->isTokenMatching($token, Token::PUNCTUATION_TYPE, self::NO_SPACE_BEFORE)) {
             return 0;
         }
 
@@ -36,12 +40,19 @@ final class PunctuationSpacingSniff extends AbstractSpacingSniff
     protected function shouldHaveSpaceAfter(int $tokenPosition, array $tokens): ?int
     {
         $token = $tokens[$tokenPosition];
-        if ($this->isTokenMatching($token, Token::PUNCTUATION_TYPE, [':', ','])) {
-            return 1;
+
+        if ($this->isTokenMatching($token, Token::PUNCTUATION_TYPE, self::NO_SPACE_AFTER)) {
+            return 0;
         }
 
-        if ($this->isTokenMatching($token, Token::PUNCTUATION_TYPE, ['(', '[', '{', '.', '|'])) {
-            return 0;
+        if ($this->isTokenMatching($token, Token::PUNCTUATION_TYPE, self::ONE_SPACE_AFTER)) {
+            // We cannot add one space after, if the next token need zero space before: `[1,2,3,]`.
+            $nextPosition = $this->findNext(Token::WHITESPACE_TOKENS, $tokens, $tokenPosition + 1, true);
+            if (false !== $nextPosition && null !== $this->shouldHaveSpaceBefore($nextPosition, $tokens)) {
+                return null;
+            }
+
+            return 1;
         }
 
         return null;
