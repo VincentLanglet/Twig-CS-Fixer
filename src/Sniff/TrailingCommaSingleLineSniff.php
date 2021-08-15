af8eda818b22501f46ee640f0ca4b6915e61aaa5
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-08-15 22:41:57 +0200

    Add TrailingCommaSingleLineSniff

---
+++ src/Sniff/TrailingCommaSingleLineSniff.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TwigCsFixer\Sniff;
+
+use Exception;
+use TwigCsFixer\Token\Token;
+
+/**
+ * Ensure that single-line arrays, objects and arguments list does not have a trailing comma.
+ */
+final class TrailingCommaSingleLineSniff extends AbstractSniff
+{
+    /**
+     * @param int               $tokenPosition
+     * @param array<int, Token> $tokens
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    protected function process(int $tokenPosition, array $tokens): void
+    {
+        $token = $tokens[$tokenPosition];
+        if (!$this->isTokenMatching($token, Token::PUNCTUATION_TYPE, [')', '}', ']'])) {
+            return;
+        }
+
+        $relatedToken = $token->getRelatedToken();
+        \assert(null !== $relatedToken); // A closer must have a related token.
+
+        if ($relatedToken->getLine() !== $token->getLine()) {
+            // Multiline.
+            return;
+        }
+
+        $previousPosition = $this->findPrevious(Token::EMPTY_TOKENS, $tokens, $tokenPosition - 1, true);
+        \assert(false !== $previousPosition); // A closer cannot be the first token.
+
+        if (!$this->isTokenMatching($tokens[$previousPosition], Token::PUNCTUATION_TYPE, ',')) {
+            return;
+        }
+
+        $fixer = $this->addFixableError(
+            'Single-line arrays, objects and parameters lists should not have trailing comma.',
+            $token
+        );
+
+        if (null === $fixer) {
+            return;
+        }
+
+        $fixer->replaceToken($previousPosition, '');
+    }
+}
