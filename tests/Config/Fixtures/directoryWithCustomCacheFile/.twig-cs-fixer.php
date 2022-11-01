686238149311f092865e86e9950b16d9ff1affee
Vincent Langlet <vincentlanglet@hotmail.fr>
2022-11-01 21:39:21 +0100

    Add tests

--- tests/Config/Fixtures/directoryWithCustomCacheFile/.twig-cs-fixer.php
+++ tests/Config/Fixtures/directoryWithCustomCacheFile/.twig-cs-fixer.php
@@ -4,6 +4,6 @@ use TwigCsFixer\Cache\NullCacheManager;
 use TwigCsFixer\Config\Config;
 
 $config = new Config('Custom');
-$config->setCacheFile(__DIR__.\DIRECTORY_SEPARATOR.'.twig-cs-fixer.cache');
+$config->setCacheFile(__DIR__.'/.twig-cs-fixer.cache');
 
 return $config;
