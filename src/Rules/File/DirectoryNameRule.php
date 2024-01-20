1839f884dcdcfeefecdb9c18faad3193af3f01e1
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-01-20 01:38:36 +0100

    Fix punctuation (#169)

--- src/Rules/File/DirectoryNameRule.php
+++ src/Rules/File/DirectoryNameRule.php
@@ -62,7 +62,7 @@ final class DirectoryNameRule extends AbstractRule implements ConfigurableRuleIn
 
             if ($expected !== $directory) {
                 $this->addFileError(
-                    sprintf('The directory name must use %s ; expected %s.', $this->case, $expected),
+                    sprintf('The directory name must use %s; expected %s.', $this->case, $expected),
                     $token,
                 );
             }
