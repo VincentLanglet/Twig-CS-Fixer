4a1e2a4dd3cf74a34fd73ea5998ba41966883938
Simon André <smn.andre@gmail.com>
2024-01-27 12:09:59 +0100

    Simplify rule

--- src/Rules/File/FileExtensionRule.php
+++ src/Rules/File/FileExtensionRule.php
@@ -6,30 +6,12 @@ namespace TwigCsFixer\Rules\File;
 
 use TwigCsFixer\File\FileHelper;
 use TwigCsFixer\Rules\AbstractRule;
-use TwigCsFixer\Rules\ConfigurableRuleInterface;
 
 /**
  * Ensures file name uses two extensions (e.g. index.html.twig).
  */
-final class FileExtensionRule extends AbstractRule implements ConfigurableRuleInterface
+final class FileExtensionRule extends AbstractRule
 {
-    /**
-     * @param array<string> $ignoredSubDirectories
-     */
-    public function __construct(
-        private ?string $baseDirectory = null,
-        private array $ignoredSubDirectories = [],
-    ) {
-    }
-
-    public function getConfiguration(): array
-    {
-        return [
-            'baseDirectory'         => $this->baseDirectory,
-            'ignoredSubDirectories' => $this->ignoredSubDirectories,
-        ];
-    }
-
     protected function process(int $tokenPosition, array $tokens): void
     {
         if (0 !== $tokenPosition) {
@@ -39,8 +21,6 @@ final class FileExtensionRule extends AbstractRule implements ConfigurableRuleIn
         $token = $tokens[$tokenPosition];
         $fileName = FileHelper::getFileName(
             $token->getFilename(),
-            $this->baseDirectory,
-            $this->ignoredSubDirectories,
         );
         if (null === $fileName) {
             return;
@@ -49,7 +29,6 @@ final class FileExtensionRule extends AbstractRule implements ConfigurableRuleIn
         $fileParts = explode('.', $fileName);
         $fileExtension = array_pop($fileParts);
         if ('twig' !== $fileExtension) {
-            // Not a Twig file: skip
             return;
         }
 
@@ -57,15 +36,6 @@ final class FileExtensionRule extends AbstractRule implements ConfigurableRuleIn
         if ([] === $fileParts || null === $formatExtension) {
             $this->addFileError(
                 sprintf('The file must use two extensions; found ".%s".', $fileExtension),
-                $token,
-            );
-
-            return;
-        }
-
-        if (1 !== preg_match('/^[a-z][a-z0-9]{1,3}$/', $formatExtension)) {
-            $this->addFileError(
-                sprintf('The file must use a valid format extension; found ".%s.%s".', $formatExtension, $fileExtension),
                 $token,
             );
         }
