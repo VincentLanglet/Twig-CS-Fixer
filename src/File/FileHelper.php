e096ea2eb0bbe177d9da598b13c7d814a9016601
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-02-08 12:32:58 +0100

    Improve support of relative path (#188)

--- src/File/FileHelper.php
+++ src/File/FileHelper.php
@@ -19,11 +19,11 @@ final class FileHelper
      * @param array<string> $ignoredDir
      */
     public static function getFileName(
-        string $absolutePath,
+        string $path,
         ?string $baseDir = null,
         array $ignoredDir = []
     ): ?string {
-        $split = self::splitPath($absolutePath, $baseDir, $ignoredDir);
+        $split = self::splitPath($path, $baseDir, $ignoredDir);
         if ([] === $split) {
             return null;
         }
@@ -37,11 +37,11 @@ final class FileHelper
      * @return list<string>
      */
     public static function getDirectories(
-        string $absolutePath,
+        string $path,
         ?string $baseDir = null,
         array $ignoredDir = []
     ): array {
-        $split = self::splitPath($absolutePath, $baseDir, $ignoredDir);
+        $split = self::splitPath($path, $baseDir, $ignoredDir);
         array_pop($split);
 
         return $split;
@@ -53,24 +53,24 @@ final class FileHelper
      * @return list<string>
      */
     private static function splitPath(
-        string $absolutePath,
+        string $path,
         ?string $baseDir = null,
         array $ignoredDir = []
     ): array {
         $baseDir = trim($baseDir ?? '', \DIRECTORY_SEPARATOR);
 
-        if ('' === $baseDir) {
+        if ('' === $baseDir || str_starts_with($path, $baseDir.\DIRECTORY_SEPARATOR)) {
             $baseDirPosition = 0;
-            $baseDir = \DIRECTORY_SEPARATOR;
+            $baseDir .= \DIRECTORY_SEPARATOR;
         } else {
             $baseDir = \DIRECTORY_SEPARATOR.$baseDir.\DIRECTORY_SEPARATOR;
-            $baseDirPosition = strrpos($absolutePath, $baseDir);
+            $baseDirPosition = strrpos($path, $baseDir);
             if (false === $baseDirPosition) {
                 return [];
             }
         }
 
-        $path = substr($absolutePath, $baseDirPosition + \strlen($baseDir));
+        $path = substr($path, $baseDirPosition + \strlen($baseDir));
         foreach ($ignoredDir as $ignoredDirectory) {
             $ignoredDirectory = trim($ignoredDirectory, \DIRECTORY_SEPARATOR).\DIRECTORY_SEPARATOR;
             if (str_starts_with($path, $ignoredDirectory)) {
