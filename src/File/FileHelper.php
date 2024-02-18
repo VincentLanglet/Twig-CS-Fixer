5783547d1f60a713e9361f09464cb799a061d65c
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-02-18 16:36:14 +0100

    Simplify

--- src/File/FileHelper.php
+++ src/File/FileHelper.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace TwigCsFixer\File;
 
+use Symfony\Component\Filesystem\Path;
 use Webmozart\Assert\Assert;
 
 final class FileHelper
 {
     public static function getAbsolutePath(string $path, ?string $workingDir = null): string
     {
+        if (Path::isAbsolute($path)) {
+            return $path;
+        }
+
         $workingDir ??= @getcwd();
         Assert::notFalse($workingDir, 'Cannot get the current working directory.');
 
-        return self::isAbsolutePath($path) ? $path : $workingDir.\DIRECTORY_SEPARATOR.$path;
+        return $workingDir.\DIRECTORY_SEPARATOR.$path;
     }
 
     public static function removeDot(string $fileName): string
@@ -70,49 +75,20 @@ final class FileHelper
         array $ignoredDir = [],
         ?string $workingDir = null,
     ): array {
-        $baseDir = self::simplifyPath(self::getAbsolutePath($baseDir ?? '', $workingDir));
-        $path = self::simplifyPath(self::getAbsolutePath($path, $workingDir));
+        $baseDir = Path::canonicalize(self::getAbsolutePath($baseDir ?? '', $workingDir));
+        $path = Path::canonicalize(self::getAbsolutePath($path, $workingDir));
 
         if (!str_starts_with($path, $baseDir.\DIRECTORY_SEPARATOR)) {
             return [];
         }
 
         foreach ($ignoredDir as $ignoredDirectory) {
-            $ignoredDirectory = self::simplifyPath(self::getAbsolutePath($ignoredDirectory, $baseDir));
+            $ignoredDirectory = Path::canonicalize(self::getAbsolutePath($ignoredDirectory, $baseDir));
             if (str_starts_with($path, $ignoredDirectory.\DIRECTORY_SEPARATOR)) {
                 return [];
             }
         }
 
         return explode(\DIRECTORY_SEPARATOR, substr($path, \strlen($baseDir) + 1));
-    }
-
-    private static function isAbsolutePath(string $path): bool
-    {
-        return '' !== $path && (
-            '/' === $path[0]
-            || '\\' === $path[0]
-            || 1 === preg_match('#^[a-zA-Z]:\\\\#', $path)
-        );
-    }
-
-    private static function simplifyPath(string $absolutePath): string
-    {
-        if (!self::isAbsolutePath($absolutePath)) {
-            throw new \InvalidArgumentException('The path must be absolute.');
-        }
-
-        $parts = explode(\DIRECTORY_SEPARATOR, $absolutePath);
-
-        $result = [];
-        foreach ($parts as $part) {
-            if ('..' === $part) {
-                array_pop($result);
-            } elseif ('.' !== $part && '' !== $part) {
-                $result[] = $part;
-            }
-        }
-
-        return \DIRECTORY_SEPARATOR.implode(\DIRECTORY_SEPARATOR, $result);
     }
 }
