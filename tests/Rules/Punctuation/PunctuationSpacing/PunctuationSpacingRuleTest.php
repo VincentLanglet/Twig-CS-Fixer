9716b5955927f062bfa31027e0d879f8c412b95a
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-04-16 12:37:10 +0200

    Add config for PunctuationSpacingRule (#208)

--- tests/Rules/Punctuation/PunctuationSpacing/PunctuationSpacingRuleTest.php
+++ tests/Rules/Punctuation/PunctuationSpacing/PunctuationSpacingRuleTest.php
@@ -9,6 +9,57 @@ use TwigCsFixer\Tests\Rules\AbstractRuleTestCase;
 
 final class PunctuationSpacingRuleTest extends AbstractRuleTestCase
 {
+    public function testConfiguration(): void
+    {
+        static::assertSame(
+            [
+                'before' => [
+                    ')' => 0,
+                    ']' => 0,
+                    '}' => 0,
+                    ':' => 0,
+                    '.' => 0,
+                    ',' => 0,
+                    '|' => 0,
+                ],
+                'after' => [
+                    '(' => 0,
+                    '[' => 0,
+                    '{' => 0,
+                    '.' => 0,
+                    '|' => 0,
+                    ':' => 1,
+                    ',' => 1,
+                ],
+            ],
+            (new PunctuationSpacingRule())->getConfiguration()
+        );
+
+        static::assertSame(
+            [
+                'before' => [
+                    '}' => null,
+                    ')' => 0,
+                    ']' => 0,
+                    ':' => 0,
+                    '.' => 0,
+                    ',' => 0,
+                    '|' => 0,
+                ],
+                'after' => [
+                    '{' => null,
+                    '(' => 0,
+                    '[' => 0,
+                    '.' => 0,
+                    '|' => 0,
+                    ':' => 1,
+                    ',' => 1,
+                ],
+            ],
+            (new PunctuationSpacingRule(['}' => null], ['{' => null]))->getConfiguration()
+        );
+    }
+
     public function testRule(): void
     {
         $this->checkRule(new PunctuationSpacingRule(), [
@@ -28,5 +79,23 @@ final class PunctuationSpacingRuleTest extends AbstractRuleTestCase
             'PunctuationSpacing.Before:7:12' => 'Expecting 0 whitespace before "}"; found 1',
             'PunctuationSpacing.Before:7:15' => 'Expecting 0 whitespace before "]"; found 1',
         ]);
+    }
+
+    public function testRuleWithConfiguration(): void
+    {
+        $this->checkRule(new PunctuationSpacingRule(['}' => null], ['{' => null]), [
+            'PunctuationSpacing.After:3:4' => 'Expecting 0 whitespace after "("; found 1',
+            'PunctuationSpacing.Before:3:10' => 'Expecting 0 whitespace before ")"; found 1',
+            'PunctuationSpacing.Before:4:10' => 'Expecting 0 whitespace before ":"; found 1',
+            'PunctuationSpacing.Before:4:16' => 'Expecting 0 whitespace before ","; found 1',
+            'PunctuationSpacing.Before:4:22' => 'Expecting 0 whitespace before ":"; found 1',
+            'PunctuationSpacing.After:5:12' => 'Expecting 0 whitespace after "["; found 1',
+            'PunctuationSpacing.Before:5:16' => 'Expecting 0 whitespace before ","; found 1',
+            'PunctuationSpacing.Before:5:20' => 'Expecting 0 whitespace before ","; found 1',
+            'PunctuationSpacing.Before:5:24' => 'Expecting 0 whitespace before "]"; found 1',
+            'PunctuationSpacing.After:6:6' => 'Expecting 0 whitespace after "|"; found 1',
+            'PunctuationSpacing.Before:6:6' => 'Expecting 0 whitespace before "|"; found 1',
+            'PunctuationSpacing.Before:7:15' => 'Expecting 0 whitespace before "]"; found 1',
+        ], fixedFilePath: false);
     }
 }
