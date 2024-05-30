98642b44a5719f18bb170c74334f50eae6a3ad12
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-05-30 10:58:58 +0200

    Allow to use tab for indentation

--- tests/Rules/Whitespace/Indent/IndentRuleTest.php
+++ tests/Rules/Whitespace/Indent/IndentRuleTest.php
@@ -11,8 +11,15 @@ final class IndentRuleTest extends AbstractRuleTestCase
 {
     public function testConfiguration(): void
     {
-        static::assertSame(['spaceRatio' => 4], (new IndentRule())->getConfiguration());
-        static::assertSame(['spaceRatio' => 2], (new IndentRule(2))->getConfiguration());
+        static::assertSame([
+            'spaceRatio' => 4,
+            'useTab' => false,
+        ], (new IndentRule())->getConfiguration());
+
+        static::assertSame([
+            'spaceRatio' => 2,
+            'useTab' => true,
+        ], (new IndentRule(2, true))->getConfiguration());
     }
 
     public function testRule(): void
@@ -33,6 +40,19 @@ final class IndentRuleTest extends AbstractRuleTestCase
             ],
             __DIR__.'/IndentRuleTest.twig',
             __DIR__.'/IndentRuleTest.fixed2.twig',
+        );
+    }
+
+    public function testRuleWithTab(): void
+    {
+        $this->checkRule(
+            new IndentRule(useTab: true),
+            [
+                'Indent.Error:12:1' => 'A file must not be indented with spaces.',
+                'Indent.Error:14:1' => 'A file must not be indented with spaces.',
+            ],
+            __DIR__.'/IndentRuleTest.tab.twig',
+            __DIR__.'/IndentRuleTest.tab.fixed.twig',
         );
     }
 }
