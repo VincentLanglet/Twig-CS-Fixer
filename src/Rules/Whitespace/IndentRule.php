f0dbee1b0362f86c0e3440ddef0f447f739a326a
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-06-16 14:30:18 +0200

    Allow to use tab for indentation (#236)

--- src/Rules/Whitespace/IndentRule.php
+++ src/Rules/Whitespace/IndentRule.php
@@ -9,22 +9,37 @@ use TwigCsFixer\Rules\ConfigurableRuleInterface;
 use TwigCsFixer\Token\Token;
 
 /**
- * Ensures that files are not indented with tabs.
+ * Ensures that files are indented with spaces (or tabs).
  */
 final class IndentRule extends AbstractFixableRule implements ConfigurableRuleInterface
 {
-    public function __construct(private int $spaceRatio = 4)
-    {
+    public function __construct(
+        private int $spaceRatio = 4,
+        private bool $useTab = false,
+    ) {
     }
 
     public function getConfiguration(): array
     {
         return [
             'spaceRatio' => $this->spaceRatio,
+            'useTab' => $this->useTab,
         ];
     }
 
     protected function process(int $tokenPosition, array $tokens): void
+    {
+        if ($this->useTab) {
+            $this->spaceToTab($tokenPosition, $tokens);
+        } else {
+            $this->tabToSpace($tokenPosition, $tokens);
+        }
+    }
+
+    /**
+     * @param array<int, Token> $tokens
+     */
+    private function tabToSpace(int $tokenPosition, array $tokens): void
     {
         $token = $tokens[$tokenPosition];
         if (!$this->isTokenMatching($token, Token::TAB_TOKENS)) {
@@ -39,6 +54,35 @@ final class IndentRule extends AbstractFixableRule implements ConfigurableRuleIn
         $fixer->replaceToken(
             $tokenPosition,
             str_replace("\t", str_repeat(' ', $this->spaceRatio), $token->getValue())
+        );
+    }
+
+    /**
+     * @param array<int, Token> $tokens
+     */
+    private function spaceToTab(int $tokenPosition, array $tokens): void
+    {
+        $token = $tokens[$tokenPosition];
+        if (1 !== $token->getPosition()) {
+            return;
+        }
+
+        if (!$this->isTokenMatching($token, Token::WHITESPACE_TOKENS)) {
+            return;
+        }
+
+        if (\strlen($token->getValue()) < $this->spaceRatio) {
+            return;
+        }
+
+        $fixer = $this->addFixableError('A file must not be indented with spaces.', $token);
+        if (null === $fixer) {
+            return;
+        }
+
+        $fixer->replaceToken(
+            $tokenPosition,
+            str_replace(str_repeat(' ', $this->spaceRatio), "\t", $token->getValue())
         );
     }
 }
