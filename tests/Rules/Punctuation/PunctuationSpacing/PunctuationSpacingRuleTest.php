af10cafe4a613b33146d0195b69afe6e0b60a2a9
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-07-04 09:10:32 +0200

    Add `skipIfNewLine` config to DelimiterSpacingRule
    Co-Authored-By: Vincent Langlet <vincentlanglet@hotmail.fr>

--- tests/Rules/Punctuation/PunctuationSpacing/PunctuationSpacingRuleTest.php
+++ tests/Rules/Punctuation/PunctuationSpacing/PunctuationSpacingRuleTest.php
@@ -63,39 +63,39 @@ final class PunctuationSpacingRuleTest extends AbstractRuleTestCase
     public function testRule(): void
     {
         $this->checkRule(new PunctuationSpacingRule(), [
-            'PunctuationSpacing.After:3:4' => 'Expecting 0 whitespace after "("; found 1',
-            'PunctuationSpacing.Before:3:10' => 'Expecting 0 whitespace before ")"; found 1',
-            'PunctuationSpacing.After:4:4' => 'Expecting 0 whitespace after "{"; found 1',
-            'PunctuationSpacing.Before:4:10' => 'Expecting 0 whitespace before ":"; found 1',
-            'PunctuationSpacing.Before:4:16' => 'Expecting 0 whitespace before ","; found 1',
-            'PunctuationSpacing.Before:4:22' => 'Expecting 0 whitespace before ":"; found 1',
-            'PunctuationSpacing.Before:4:28' => 'Expecting 0 whitespace before "}"; found 1',
-            'PunctuationSpacing.After:5:12' => 'Expecting 0 whitespace after "["; found 1',
-            'PunctuationSpacing.Before:5:16' => 'Expecting 0 whitespace before ","; found 1',
-            'PunctuationSpacing.Before:5:20' => 'Expecting 0 whitespace before ","; found 1',
-            'PunctuationSpacing.Before:5:24' => 'Expecting 0 whitespace before "]"; found 1',
-            'PunctuationSpacing.After:6:6' => 'Expecting 0 whitespace after "|"; found 1',
-            'PunctuationSpacing.Before:6:6' => 'Expecting 0 whitespace before "|"; found 1',
-            'PunctuationSpacing.Before:7:12' => 'Expecting 0 whitespace before "}"; found 1',
-            'PunctuationSpacing.Before:7:15' => 'Expecting 0 whitespace before "]"; found 1',
+            'PunctuationSpacing.After:3:4' => 'Expecting 0 whitespace after "("; found 1.',
+            'PunctuationSpacing.Before:3:10' => 'Expecting 0 whitespace before ")"; found 1.',
+            'PunctuationSpacing.After:4:4' => 'Expecting 0 whitespace after "{"; found 1.',
+            'PunctuationSpacing.Before:4:10' => 'Expecting 0 whitespace before ":"; found 1.',
+            'PunctuationSpacing.Before:4:16' => 'Expecting 0 whitespace before ","; found 1.',
+            'PunctuationSpacing.Before:4:22' => 'Expecting 0 whitespace before ":"; found 1.',
+            'PunctuationSpacing.Before:4:28' => 'Expecting 0 whitespace before "}"; found 1.',
+            'PunctuationSpacing.After:5:12' => 'Expecting 0 whitespace after "["; found 1.',
+            'PunctuationSpacing.Before:5:16' => 'Expecting 0 whitespace before ","; found 1.',
+            'PunctuationSpacing.Before:5:20' => 'Expecting 0 whitespace before ","; found 1.',
+            'PunctuationSpacing.Before:5:24' => 'Expecting 0 whitespace before "]"; found 1.',
+            'PunctuationSpacing.After:6:6' => 'Expecting 0 whitespace after "|"; found 1.',
+            'PunctuationSpacing.Before:6:6' => 'Expecting 0 whitespace before "|"; found 1.',
+            'PunctuationSpacing.Before:7:12' => 'Expecting 0 whitespace before "}"; found 1.',
+            'PunctuationSpacing.Before:7:15' => 'Expecting 0 whitespace before "]"; found 1.',
         ]);
     }
 
     public function testRuleWithConfiguration(): void
     {
         $this->checkRule(new PunctuationSpacingRule(['}' => null], ['{' => null]), [
-            'PunctuationSpacing.After:3:4' => 'Expecting 0 whitespace after "("; found 1',
-            'PunctuationSpacing.Before:3:10' => 'Expecting 0 whitespace before ")"; found 1',
-            'PunctuationSpacing.Before:4:10' => 'Expecting 0 whitespace before ":"; found 1',
-            'PunctuationSpacing.Before:4:16' => 'Expecting 0 whitespace before ","; found 1',
-            'PunctuationSpacing.Before:4:22' => 'Expecting 0 whitespace before ":"; found 1',
-            'PunctuationSpacing.After:5:12' => 'Expecting 0 whitespace after "["; found 1',
-            'PunctuationSpacing.Before:5:16' => 'Expecting 0 whitespace before ","; found 1',
-            'PunctuationSpacing.Before:5:20' => 'Expecting 0 whitespace before ","; found 1',
-            'PunctuationSpacing.Before:5:24' => 'Expecting 0 whitespace before "]"; found 1',
-            'PunctuationSpacing.After:6:6' => 'Expecting 0 whitespace after "|"; found 1',
-            'PunctuationSpacing.Before:6:6' => 'Expecting 0 whitespace before "|"; found 1',
-            'PunctuationSpacing.Before:7:15' => 'Expecting 0 whitespace before "]"; found 1',
+            'PunctuationSpacing.After:3:4' => 'Expecting 0 whitespace after "("; found 1.',
+            'PunctuationSpacing.Before:3:10' => 'Expecting 0 whitespace before ")"; found 1.',
+            'PunctuationSpacing.Before:4:10' => 'Expecting 0 whitespace before ":"; found 1.',
+            'PunctuationSpacing.Before:4:16' => 'Expecting 0 whitespace before ","; found 1.',
+            'PunctuationSpacing.Before:4:22' => 'Expecting 0 whitespace before ":"; found 1.',
+            'PunctuationSpacing.After:5:12' => 'Expecting 0 whitespace after "["; found 1.',
+            'PunctuationSpacing.Before:5:16' => 'Expecting 0 whitespace before ","; found 1.',
+            'PunctuationSpacing.Before:5:20' => 'Expecting 0 whitespace before ","; found 1.',
+            'PunctuationSpacing.Before:5:24' => 'Expecting 0 whitespace before "]"; found 1.',
+            'PunctuationSpacing.After:6:6' => 'Expecting 0 whitespace after "|"; found 1.',
+            'PunctuationSpacing.Before:6:6' => 'Expecting 0 whitespace before "|"; found 1.',
+            'PunctuationSpacing.Before:7:15' => 'Expecting 0 whitespace before "]"; found 1.',
         ], fixedFilePath: false);
     }
 }
