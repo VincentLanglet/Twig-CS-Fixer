2bd4b22ee25ab28391906b88a23dea2196072800
Laurent Laville <laurent.laville@gmail.com>
2024-07-13 05:27:30 +0000

    fixes psalm issues

--- src/Report/Reporter/CheckstyleReporter.php
+++ src/Report/Reporter/CheckstyleReporter.php
@@ -23,8 +23,8 @@ final class CheckstyleReporter implements ReporterInterface
         $text .= '<checkstyle>'.\PHP_EOL;
 
         foreach ($report->getFiles(true) as $file => $realPath) {
-            /** @var string $file */
             /** @var string $realPath */
+            /** @var string $file */
             $fileViolations = $report->getFileViolations($file, $level);
             if (0 === \count($fileViolations)) {
                 continue;
