75c175a0bc823ddaa4afee65135ffaeca376a9a4
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-07-21 00:17:07 +0200

    Working

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,10 +2,27 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Twig\Environment;
+use Twig\Extension\ExtensionInterface;
+use Twig\NodeVisitor\NodeVisitorInterface;
+use Twig\Source;
+use Twig\TokenParser\TokenParserInterface;
+
 return [
-    'exclude-namespaces' => ['^TwigCsFixer\\'],
+    'exclude-namespaces' => ['TwigCsFixer'],
     'expose-global-constants' => false,
     'expose-global-classes' => false,
     'expose-global-functions' => false,
-    'expose-namespaces' => ['^Symfony\\', '^Twig\\'],
+    'expose-namespaces' => ['Twig\Node'],
+    'expose-classes' => [
+        OutputInterface::class,
+        Finder::class,
+        Environment::class,
+        ExtensionInterface::class,
+        NodeVisitorInterface::class,
+        Source::class,
+        TokenParserInterface::class,
+    ],
 ];
