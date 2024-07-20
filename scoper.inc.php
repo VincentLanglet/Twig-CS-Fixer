9830c4853ec23c357037b43bc688d7ab4bcf186f
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-07-20 22:54:17 +0200

    Try

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,17 +2,10 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Finder\Finder;
-use Twig\Extension\ExtensionInterface;
-use Twig\NodeVisitor\NodeVisitorInterface;
-use Twig\TokenParser\TokenParserInterface;
-
 return [
-    'exclude-namespaces' => ['TwigCsFixer'],
-    'expose-classes' => [
-        Finder::class,
-        ExtensionInterface::class,
-        NodeVisitorInterface::class,
-        TokenParserInterface::class,
-    ],
+    'exclude-namespaces' => ['^TwigCsFixer\\'],
+    'expose-global-constants' => false,
+    'expose-global-classes' => false,
+    'expose-global-functions' => false,
+    'expose-namespaces' => ['^Symfony\\', '^Twig\\'],
 ];
