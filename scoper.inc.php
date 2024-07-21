5846e151e68bc73258508066179ad93828b98286
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-07-21 16:35:40 +0200

    Add test for phar (#281)

--- scoper.inc.php
+++ scoper.inc.php
@@ -4,25 +4,15 @@ declare(strict_types=1);
 
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
-use Twig\Environment;
-use Twig\Extension\ExtensionInterface;
-use Twig\NodeVisitor\NodeVisitorInterface;
-use Twig\Source;
-use Twig\TokenParser\TokenParserInterface;
 
 return [
     'exclude-namespaces' => ['TwigCsFixer'],
     'expose-global-constants' => false,
     'expose-global-classes' => false,
     'expose-global-functions' => false,
-    'expose-namespaces' => ['Twig\Node'],
+    'expose-namespaces' => ['/^Twig(?!\\\\Test(\\\\|$))/'],
     'expose-classes' => [
-        OutputInterface::class,
         Finder::class,
-        Environment::class,
-        ExtensionInterface::class,
-        NodeVisitorInterface::class,
-        Source::class,
-        TokenParserInterface::class,
+        OutputInterface::class,
     ],
 ];
