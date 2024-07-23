9509febe76cf3c3590d69d36823ecfd1919a1ec9
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-07-23 09:36:12 +0200

    Improve test

--- tests/Rules/Node/ValidConstantFunction/ValidConstantFunctionRuleTest.php
+++ tests/Rules/Node/ValidConstantFunction/ValidConstantFunctionRuleTest.php
@@ -16,10 +16,12 @@ final class ValidConstantFunctionRuleTest extends AbstractRuleTestCase
         $this->checkRule(new ValidConstantFunctionRule(), [
             'ValidConstantFunction.ConstantUndefined:7' => 'Constant "ThisDoesNotExist::SomeKey" is undefined.',
             'ValidConstantFunction.ClassConstant:9' => 'You cannot use the function "constant()" to resolve class names.',
-            'ValidConstantFunction.StringConstant:10' => 'The first param of the function "constant()" must be a string.',
-            'ValidConstantFunction.ConstantUndefined:17' => 'Constant "ThisDoesNotExist::SomeKey" is undefined.',
-            'ValidConstantFunction.ClassConstant:19' => 'You cannot use the function "constant()" to resolve class names.',
-            'ValidConstantFunction.StringConstant:20' => 'The first param of the function "constant()" must be a string.',
+            'ValidConstantFunction.ClassConstant:10' => 'You cannot use the function "constant()" to resolve class names.',
+            'ValidConstantFunction.StringConstant:11' => 'The first param of the function "constant()" must be a string.',
+            'ValidConstantFunction.ConstantUndefined:18' => 'Constant "ThisDoesNotExist::SomeKey" is undefined.',
+            'ValidConstantFunction.ClassConstant:20' => 'You cannot use the function "constant()" to resolve class names.',
+            'ValidConstantFunction.ClassConstant:21' => 'You cannot use the function "constant()" to resolve class names.',
+            'ValidConstantFunction.StringConstant:22' => 'The first param of the function "constant()" must be a string.',
         ]);
     }
 }
