0a93a299700b69b2c43c9d618392275751d4da30
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-05-16 20:02:54 +0200

    Handle space in CompactHashRule

--- tests/Rules/Literal/CompactHashRule/CompactHashRuleTest.php
+++ tests/Rules/Literal/CompactHashRule/CompactHashRuleTest.php
@@ -25,13 +25,21 @@ final class CompactHashRuleTest extends AbstractRuleTestCase
     {
         $this->checkRule(new CompactHashRule(), [
             'CompactHash.Error:1:38' => 'Hash key "thing" should be explicit.',
+            'CompactHash.Error:3:20' => 'Hash key "product" should be explicit.',
+            'CompactHash.Error:5:5' => 'Hash key "product" should be explicit.',
         ]);
     }
 
-    public function testRuleWithSingleQuote(): void
+    public function testRuleWithCompact(): void
     {
         $this->checkRule(new CompactHashRule(true), [
             'CompactHash.Error:1:5' => 'Hash key "foo" should be implicit.',
+            'CompactHash.Error:8:20' => 'Hash key "product" should be implicit.',
+            'CompactHash.Error:10:5' => 'Hash key "product" should be implicit.',
+            'CompactHash.Error:13:20' => 'Hash key "product" should be implicit.',
+            'CompactHash.Error:15:5' => 'Hash key "product" should be implicit.',
+            'CompactHash.Error:18:20' => 'Hash key "product" should be implicit.',
+            'CompactHash.Error:20:5' => 'Hash key "product" should be implicit.',
         ], fixedFilePath: __DIR__.'/CompactHashRuleTest.compact.fixed.twig');
     }
 }
