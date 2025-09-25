d56c1cfdfa0f28c5d44c6dfe772623433cc39ab7
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2025-09-25 21:39:34 +0200

    Give credits to PHP CS Fixer for caching (#396)

--- src/Cache/Manager/NullCacheManager.php
+++ src/Cache/Manager/NullCacheManager.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace TwigCsFixer\Cache\Manager;
 
+/**
+ * This file was copied (and slightly modified) from PHP CS Fixer:
+ * - https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/v3.13.0/src/Cache/NullCacheManager.php
+ * - (c) Fabien Potencier <fabien@symfony.com>, Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ * - For the full copyright and license information, please see https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/v3.13.0/LICENSE
+ */
 final class NullCacheManager implements CacheManagerInterface
 {
     public function needFixing(string $file, string $fileContent): bool
