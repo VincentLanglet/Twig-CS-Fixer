030666d44ed45a9c182e97bedad7dd994d6702f9
Bibi <laurent.muller@haslerrail.com>
2025-11-23 11:28:34 +0100

    Add EndBlockNameRule.

---
+++ src/Rules/Variable/EndBlockNameRule.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TwigCsFixer\Rules\Variable;
+
+use TwigCsFixer\Rules\AbstractFixableRule;
+use TwigCsFixer\Token\Token;
+use TwigCsFixer\Token\Tokens;
+
+/**
+ * Ensures that block name is set at the end of the block.
+ */
+final class EndBlockNameRule extends AbstractFixableRule
+{
+    protected function process(int $tokenIndex, Tokens $tokens): void
+    {
+        // end block?
+        $token = $tokens->get($tokenIndex);
+        if (!$token->isMatching(Token::BLOCK_NAME_TYPE, 'endblock')) {
+            return;
+        }
+
+        // check if name is already set
+        $nameToken = $this->findNameToken($tokenIndex + 1, $tokens);
+        if ($nameToken instanceof Token) {
+            return;
+        }
+
+        // find start block
+        $indent = 0;
+        $index = $tokenIndex - 1;
+        while ($tokens->has($index)) {
+            $current = $tokens->get($index);
+            if ($current->isMatching(Token::BLOCK_NAME_TYPE, 'endblock')) {
+                ++$indent;
+                --$index;
+                continue;
+            }
+
+            if ($current->isMatching(Token::BLOCK_NAME_TYPE, 'block')) {
+                if ($indent > 0) {
+                    --$indent;
+                } elseif (0 === $indent) {
+                    break;
+                }
+            }
+            --$index;
+        }
+
+        // find name
+        $nameToken = $this->findNameToken($index + 1, $tokens);
+        if (!$nameToken instanceof Token) {
+            return;
+        }
+        $value = $nameToken->getValue();
+        $fixer = $this->addFixableError(
+            'The end block must have the "'.$value.'" name.',
+            $token
+        );
+        if (null === $fixer) {
+            return;
+        }
+        $fixer->addContent($tokenIndex, ' '.$value);
+    }
+
+    private function findNameToken(int $index, Tokens $tokens): ?Token
+    {
+        while ($tokens->has($index) && $tokens->get($index)->isMatching(Token::WHITESPACE_TOKENS)) {
+            ++$index;
+        }
+        if (!$tokens->has($index)) {
+            return null;
+        }
+        $token = $tokens->get($index);
+
+        return $token->isMatching(Token::NAME_TYPE) ? $token : null;
+    }
+}
---
+++ tests/Rules/Variable/EndBlockName/EndBlockNameRuleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Rules\Variable\EndBlockName;
+
+use TwigCsFixer\Rules\Variable\EndBlockNameRule;
+use TwigCsFixer\Test\AbstractRuleTestCase;
+
+class EndBlockNameRuleTest extends AbstractRuleTestCase
+{
+    public function testRule(): void
+    {
+        $this->checkRule(new EndBlockNameRule(), [
+            'EndBlockName.Error:2:4' => 'The end block must have the "test" name.',
+            'EndBlockName.Error:6:4' => 'The end block must have the "outer_block" name.',
+            'EndBlockName.Error:9:8' => 'The end block must have the "inner_block" name.',
+        ]);
+    }
+}
