151752e6522e57407d04f6f3d702b2982946b229
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-04-05 11:41:53 +0200

    Rework

--- src/Rules/Operator/StrictComparisonOperatorRule.php
+++ src/Rules/Operator/StrictComparisonOperatorRule.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace TwigCsFixer\Rules\Operator;
 
+use TwigCsFixer\Environment\StubbedEnvironment;
 use TwigCsFixer\Rules\AbstractFixableRule;
 use TwigCsFixer\Token\Token;
 use TwigCsFixer\Token\Tokens;
+use Webmozart\Assert\Assert;
 
 /**
  * Ensures that strict comparison operators are used instead of "same as" and "not same as".
@@ -15,37 +17,35 @@ final class StrictComparisonOperatorRule extends AbstractFixableRule
 {
     protected function process(int $tokenIndex, Tokens $tokens): void
     {
-        $token = $tokens->get($tokenIndex);
+        if (!StubbedEnvironment::satisfiesTwigVersion(3, 23)) {
+            // @codeCoverageIgnoreStart
+            return;
+            // @codeCoverageIgnoreEnd
+        }
 
+        $token = $tokens->get($tokenIndex);
         if (!$token->isMatching(Token::OPERATOR_TYPE, ['is', 'is not'])) {
             return;
         }
 
-        $isNegated = $token->isMatching(Token::OPERATOR_TYPE, 'is not');
-
         $sameIndex = $tokens->findNext(Token::EMPTY_TOKENS, $tokenIndex + 1, exclude: true);
-        if (false === $sameIndex) {
-            return;
-        }
+        Assert::notFalse($sameIndex, 'An OPERATOR_TYPE cannot be the last non-empty token');
 
         if (!$tokens->get($sameIndex)->isMatching(Token::TEST_NAME_TYPE, 'same')) {
             return;
         }
 
         $asIndex = $tokens->findNext(Token::EMPTY_TOKENS, $sameIndex + 1, exclude: true);
-        if (false === $asIndex) {
-            return;
-        }
+        Assert::notFalse($asIndex, 'A TEST_NAME_TYPE cannot be the last non-empty token');
 
         if (!$tokens->get($asIndex)->isMatching(Token::TEST_NAME_TYPE, 'as')) {
             return;
         }
 
         $nextIndex = $tokens->findNext(Token::EMPTY_TOKENS, $asIndex + 1, exclude: true);
-        if (false === $nextIndex) {
-            return;
-        }
+        Assert::notFalse($nextIndex, 'A TEST_NAME_TYPE cannot be the last non-empty token');
 
+        $isNegated = 'is not' === $token->getValue();
         $operator = $isNegated ? '!==' : '===';
         $oldOperator = $isNegated ? 'not same as' : 'same as';
 
@@ -53,38 +53,24 @@ final class StrictComparisonOperatorRule extends AbstractFixableRule
             \sprintf('Use strict comparison operator "%s" instead of "%s".', $operator, $oldOperator),
             $token
         );
-
         if (null === $fixer) {
             return;
         }
 
         $fixer->beginChangeSet();
 
-        $nextToken = $tokens->get($nextIndex);
+        $fixer->replaceToken($tokenIndex, $operator);
+        for ($i = $tokenIndex + 1; $i <= $asIndex; ++$i) {
+            $fixer->replaceToken($i, '');
+        }
 
+        $nextToken = $tokens->get($nextIndex);
         if ($nextToken->isMatching(Token::PUNCTUATION_TYPE, '(')) {
             $relatedToken = $nextToken->getRelatedToken();
-            if (null === $relatedToken) {
-                $fixer->endChangeSet();
+            Assert::notNull($relatedToken, 'An opener is always related to an closer.');
 
-                return;
-            }
-
-            $closeParenthesisIndex = $tokens->getIndex($relatedToken);
-
-            $fixer->replaceToken($tokenIndex, $operator);
-
-            for ($i = $tokenIndex + 1; $i <= $nextIndex; ++$i) {
-                $fixer->replaceToken($i, '');
-            }
-
-            $fixer->replaceToken($closeParenthesisIndex, '');
-        } else {
-            $fixer->replaceToken($tokenIndex, $operator);
-
-            for ($i = $tokenIndex + 1; $i <= $asIndex; ++$i) {
-                $fixer->replaceToken($i, '');
-            }
+            $fixer->replaceToken($nextIndex, '');
+            $fixer->replaceToken($tokens->getIndex($relatedToken), '');
         }
 
         $fixer->endChangeSet();
--- tests/Rules/Operator/StrictComparisonOperator/StrictComparisonOperatorRuleTest.php
+++ tests/Rules/Operator/StrictComparisonOperator/StrictComparisonOperatorRuleTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace TwigCsFixer\Tests\Rules\Operator\StrictComparisonOperator;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use TwigCsFixer\Rules\Operator\StrictComparisonOperatorRule;
 use TwigCsFixer\Test\AbstractRuleTestCase;
 
@@ -11,6 +13,10 @@ final class StrictComparisonOperatorRuleTest extends AbstractRuleTestCase
 {
     public function testRule(): void
     {
+        if (!InstalledVersions::satisfies(new VersionParser(), 'twig/twig', '>=3.23.0')) {
+            static::markTestSkipped('twig/twig ^3.23.0 is required.');
+        }
+
         $this->checkRule(new StrictComparisonOperatorRule(), [
             'StrictComparisonOperator.Error:1:6' => 'Use strict comparison operator "===" instead of "same as".',
             'StrictComparisonOperator.Error:2:6' => 'Use strict comparison operator "!==" instead of "not same as".',
